f10fea59081095c82045ccb746e0a944375d59ba
Yossueth <patricksarguedas2806@gmail.com>
2024-05-09 08:33:17 -0600

    sobre mi

--- src/js/login.js
+++ src/js/login.js
@@ -1,16 +1,14 @@
 const loginForm = document.querySelector('#loginForm')
-loginForm.addEventListener('submit', (e) => {
+loginForm.addEventListener('submit', (e)=>{
     e.preventDefault()
     const email = document.querySelector('#email').value
     const password = document.querySelector('#password').value
-    const Users = JSON.parse(localStorage.getItem('user')) || []
-    const validUser = Users.find(user => user.email == email && user.password == password)
-    if (!validUser) {
-        return alert('Usuario y/o contraseña incorrectos')
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const validUser = Users.find(user => user.email === email && user.password === password)
+    if(!validUser){
+        return alert('Usuario y/o contraseña incorrectos!')
     }
     alert(`Bienvenido ${validUser.name}`)
     localStorage.setItem('login_success', JSON.stringify(validUser))
-    window.location.href = 'index.html'
+    window.location.href = 'index.html'   
 })
-
-
--- src/js/signop.js
+++ src/js/signop.js
@@ -1,18 +1,19 @@
-const signopForm = document.querySelector("#signopForm");
-signopForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const name = document.querySelector("#name").value;
-  const email = document.querySelector("#email").value;
-  const password = document.querySelector("#password").value;
+const signupForm = document.querySelector('#signupForm')
+signupForm.addEventListener('submit', (e)=>{
+    e.preventDefault()
+    const name = document.querySelector('#name').value
+    const email = document.querySelector('#email').value
+    const password = document.querySelector('#password').value
 
-  const user = JSON.parse(localStorage.getItem("users")) || [];
-  const isUserRegistered = user.find((user) => user.email === email);
-  if (isUserRegistered) {
-    return alert("El usuario ya esta registrado");
-  }
+    const Users = JSON.parse(localStorage.getItem('users')) || []
+    const isUserRegistered = Users.find(user => user.email === email)
+    if(isUserRegistered){
+        return alert('El usuario ya esta registado!')
+    }
 
-  user.push({ name: name, email: email, password: password });
-  localStorage.setItem("user", JSON.stringify(user));
-  alert("Registro Exitiso");
-  window.location.href = 'login.html';
-});
+    Users.push({name: name, email: email, password: password})
+    localStorage.setItem('users', JSON.stringify(Users))
+    alert('Registro Exitoso!')
+    window.location.href = 'login.html'
+
+})
